b450715adc09629d8c701d4c0fe1249e99d5c959
Noel E. Valentin Roman <noel.valentin2@upr.edu>
2018-10-29 08:06:05 -0400

    Initial commit

---
+++ src/menuClasses/DeleteListAction.java
@@ -0,0 +1,18 @@
+package menuClasses;
+
+import dataManager.DMComponent;
+import ioManagementClasses.IOComponent;
+
+public class DeleteListAction implements Action {
+	
+	@Override 
+	public void execute(Object arg) { 
+		
+		IOComponent io = IOComponent.getComponent(); 
+		DMComponent dm = (DMComponent) arg; 
+		String name = io.getInput("Enter the name of the list to delete: ");
+		dm.deleteList(name); 		
+		
+	}
+
+}
